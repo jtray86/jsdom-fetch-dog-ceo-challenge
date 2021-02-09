b86975c3f88daa4636a93efa2d1c3f69d0eef0b1
jennifer tracy <jenniferetracy@gmail.com>
2021-02-09 17:30:30 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,74 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+const imgContainer = document.getElementById('dog-image-container')
+const breedUl = document.getElementById('dog-breeds')
+let breed
+const breedDropdown = document.getElementById('breed-dropdown')
+
+function fetchImages(){
+    fetch(imgUrl)
+    .then(res => res.json())
+    .then(data => dogData = data.message)
+    .then(dogData => dogData.forEach(dog => renderDogImg(dog)))
+}
+
+function fetchBreeds(){
+    fetch(breedUrl)
+    .then(res => res.json())
+    .then(breedOjt => {
+        
+        breed = Object.keys(breedOjt.message)
+        dogBreeds(breed)
+    })
+}
+
+
+
+function renderDogImg(dog){
+    
+    const img = document.createElement('img')
+
+    img.src = dog
+    img.alt = "dog"
+    
+    
+    imgContainer.appendChild(img)
+}
+
+function dogBreeds(breed){
+    breed.forEach(dogBreed => { 
+        const breedLi = document.createElement('li')
+
+        breedLi.innerText = dogBreed
+        breedUl.append(breedLi)
+
+        
+    })
+}
+
+breedUl.addEventListener("click", colorChanger)
+
+function colorChanger(e){
+    if (e.target.tagName === "LI"){
+        e.target.style.color = "green"
+    }
+
+}
+
+breedDropdown.addEventListener('change', dropdownHandle)
+
+function dropdownHandle(e){
+   
+    const filterBreed = breed.filter(dog => dog.startsWith(e.target.value))
+
+    while (breedUl.firstChild){
+        breedUl.lastChild.remove()
+    }
+
+    dogBreeds(filterBreed)
+}
+
+
+fetchBreeds()
+fetchImages()
